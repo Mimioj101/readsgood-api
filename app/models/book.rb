b8481938da6071e31f5da57898e02e09e1a1d381
Mimioj101 <mimioj@gmail.com>
2021-01-13 16:08:55 -0500

    testing api key

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,6 @@ class Book < ApplicationRecord
     has_many :want_to_reads
     has_many :users, through: :read_books
     has_many :users, through: :want_to_reads
+
+    GOOGLE_API_KEY = ENV['google_api_key']
 end
